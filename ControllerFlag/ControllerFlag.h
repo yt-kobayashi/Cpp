444e473feb2f8dd1bba9045b62a430c04a719a31
yt-kobayashi <32455802+yt-kobayashi@users.noreply.github.com>
2018-12-09 04:43:53 +0900

    コントローラ用フラグ構造体

---
+++ ControllerFlag/ControllerFlag.h
@@ -0,0 +1,21 @@
+#pragma once
+struct CONTROLLERFLAG {
+	long moveFlag;
+	long manualFlag;
+	long autoFlag;
+	long getlogFlag;
+	long xDirectionFlag;
+	long yDirectionFlag;
+	long xTimeValue;
+	long yTimeValue;
+	long xMoveFlag;
+	long yMoveFlag;
+	long outputVoltage;
+	long analogOut0;
+	long analogOut1;
+	long analogIn0;
+	long analogIn1;
+	long analogIn2;
+	long analogIn3;
+	long outMoveFlag;
+};
